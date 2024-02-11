16c455869a8dfd85c593ff5b17e21b16934e4cde
MaxDimasDev <mrcrzy01818@gmail.com>
2024-02-11 15:45:37 -0600

    fix: .js files modified to work with .json file

--- HW2/array_methdos_api.js
+++ HW2/array_methdos_api.js
@@ -1,35 +1,33 @@
-const books = [
-    { title: "Book1", isbn: "111", genre: "Fiction", stock: true },
-    { title: "Book2", isbn: "222", genre: "Non-Fiction", stock: false },
-    { title: "Book3", isbn: "333", genre: "Fiction", stock: true },
-];
+const fs = require('fs');
+const booksData = JSON.parse(fs.readFileSync('books.json'));
 
 function formatResponse(data) {
     return { code: data.code, data: data.data };
 }
 
 function getBook(titleOrIsbn) {
-    const book = books.find((book) => book.title === titleOrIsbn || book.isbn === titleOrIsbn);
+    const book = booksData.find((book) => book.title === titleOrIsbn || book.ISBN === titleOrIsbn);
     return formatResponse({ code: 200, data: book });
 }
 
 function getBooks() {
-    return formatResponse({ code: 200, data: books });
+    return formatResponse({ code: 200, data: booksData });
 }
 
 function addBook(newBook) {
-    const addedBook = { ...newBook, id: books.length + 1 };
-    books.push(addedBook);
-    return formatResponse({ code: 201, data: [addedBook, books] });
+    const addedBook = { ...newBook, id: booksData.length + 1 };
+    booksData.push(addedBook);
+    return formatResponse({ code: 201, data: [addedBook, booksData] });
 }
 
 function removeBookByTitleOrISBN(titleOrIsbn) {
-    const [deletedBook, newBooks] = removeElement(books, (book) => book.title === titleOrIsbn || book.isbn === titleOrIsbn);
-    return formatResponse({ code: 200, data: { deletedBook, newBooks } });
+    const index = booksData.findIndex((book) => book.title === titleOrIsbn || book.ISBN === titleOrIsbn);
+    const deletedBook = booksData.splice(index, 1)[0];
+    return formatResponse({ code: 200, data: { deletedBook, newBooks: booksData } });
 }
 
 function filterBy(filterProperty, filterString) {
-    const filteredBooks = books.filter((book) => {
+    const filteredBooks = booksData.filter((book) => {
         if (book[filterProperty]) {
             return book[filterProperty].toLowerCase().includes(filterString.toLowerCase());
         }
@@ -39,48 +37,41 @@ function filterBy(filterProperty, filterString) {
 }
 
 function listBooks() {
-    const listedBooks = books.map((book) => `${book.title} - ${book.author} - ${book.year}`);
+    const listedBooks = booksData.map((book) => `${book.title} - ${book.author} - ${book.year}`);
     return formatResponse({ code: 200, data: listedBooks });
 }
 
 function getBooksByYear(year) {
-    const booksByYear = books.filter((book) => book.year === year);
+    const booksByYear = booksData.filter((book) => book.year === year);
     return formatResponse({ code: 200, data: booksByYear });
 }
 
 function genreFullAvailability(genre) {
-    const allAvailable = books.every((book) => book.genre === genre && book.stock);
+    const allAvailable = booksData.every((book) => book.genre === genre && book.stock);
     return formatResponse({ code: 200, data: allAvailable });
 }
 
 function genrePartialAvailability(genre) {
-    const atLeastOneAvailable = books.some((book) => book.genre === genre && book.stock);
+    const atLeastOneAvailable = booksData.some((book) => book.genre === genre && book.stock);
     return formatResponse({ code: 200, data: atLeastOneAvailable });
 }
 
 function getCountBy(countProperty) {
-    const count = books.reduce((counter, book) => {
+    const count = booksData.reduce((counter, book) => {
         counter[book[countProperty]] = (counter[book[countProperty]] || 0) + 1;
         return counter;
     }, {});
     return formatResponse({ code: 200, data: count });
 }
 
-// Helper function to remove an element from an array
-function removeElement(array, predicate) {
-    const index = array.findIndex(predicate);
-    if (index === -1) return [null, array];
-    return [array.splice(index, 1)[0], array];
-}
-
 // Test functions
-console.log(getBook("111"));
+console.log(getBook("9781408855652"));
 console.log(getBooks());
-console.log(addBook({ title: "Oaxacos por el mundo", author: "Mond Oaxaca", year: 2022 }));
-console.log(removeBookByTitleOrISBN("111"));
-console.log(filterBy("genre", "Fiction"));
+console.log(addBook({ title: "New Book", ISBN: "1234567890", year: 2024 }));
+console.log(removeBookByTitleOrISBN("9781408855652"));
+console.log(filterBy("genre", "Fantasy"));
 console.log(listBooks());
-console.log(getBooksByYear(2022));
-console.log(genreFullAvailability("Fiction"));
+console.log(getBooksByYear(2018));
+console.log(genreFullAvailability("Self-help"));
 console.log(genrePartialAvailability("Non-Fiction"));
 console.log(getCountBy("genre"));
--- HW2/arrow_array_methods_api.js
+++ HW2/arrow_array_methods_api.js
@@ -1,31 +1,29 @@
-const books = [
-    { title: "Book1", isbn: "111", genre: "Fiction", stock: true },
-    { title: "Book2", isbn: "222", genre: "Non-Fiction", stock: false },
-    { title: "Book3", isbn: "333", genre: "Fiction", stock: true },
-];
+const fs = require('fs');
+const booksData = JSON.parse(fs.readFileSync('books.json'));
 
 const formatResponse = (data) => ({ code: data.code, data: data.data });
 
 const getBook = (titleOrIsbn) => {
-    const book = books.find((book) => book.title === titleOrIsbn || book.isbn === titleOrIsbn);
+    const book = booksData.find((book) => book.title === titleOrIsbn || book.ISBN === titleOrIsbn);
     return formatResponse({ code: 200, data: book });
 };
 
-const getBooks = () => formatResponse({ code: 200, data: books });
+const getBooks = () => formatResponse({ code: 200, data: booksData });
 
 const addBook = (newBook) => {
-    const addedBook = { ...newBook, id: books.length + 1 };
-    books.push(addedBook);
-    return formatResponse({ code: 201, data: [addedBook, books] });
+    const addedBook = { ...newBook, id: booksData.length + 1 };
+    booksData.push(addedBook);
+    return formatResponse({ code: 201, data: [addedBook, booksData] });
 };
 
 const removeBookByTitleOrISBN = (titleOrIsbn) => {
-    const [deletedBook, newBooks] = removeElement(books, (book) => book.title === titleOrIsbn || book.isbn === titleOrIsbn);
-    return formatResponse({ code: 200, data: { deletedBook, newBooks } });
+    const index = booksData.findIndex((book) => book.title === titleOrIsbn || book.ISBN === titleOrIsbn);
+    const deletedBook = booksData.splice(index, 1)[0];
+    return formatResponse({ code: 200, data: { deletedBook, newBooks: booksData } });
 };
 
 const filterBy = (filterProperty, filterString) => {
-    const filteredBooks = books.filter((book) => {
+    const filteredBooks = booksData.filter((book) => {
         if (book[filterProperty]) {
             return book[filterProperty].toLowerCase().includes(filterString.toLowerCase());
         }
@@ -35,48 +33,41 @@ const filterBy = (filterProperty, filterString) => {
 };
 
 const listBooks = () => {
-    const listedBooks = books.map((book) => `${book.title} - ${book.author} - ${book.year}`);
+    const listedBooks = booksData.map((book) => `${book.title} - ${book.author} - ${book.year}`);
     return formatResponse({ code: 200, data: listedBooks });
 };
 
 const getBooksByYear = (year) => {
-    const booksByYear = books.filter((book) => book.year === year);
+    const booksByYear = booksData.filter((book) => book.year === year);
     return formatResponse({ code: 200, data: booksByYear });
 };
 
 const genreFullAvailability = (genre) => {
-    const allAvailable = books.every((book) => book.genre === genre && book.stock);
+    const allAvailable = booksData.every((book) => book.genre === genre && book.stock);
     return formatResponse({ code: 200, data: allAvailable });
 };
 
 const genrePartialAvailability = (genre) => {
-    const atLeastOneAvailable = books.some((book) => book.genre === genre && book.stock);
+    const atLeastOneAvailable = booksData.some((book) => book.genre === genre && book.stock);
     return formatResponse({ code: 200, data: atLeastOneAvailable });
 };
 
 const getCountBy = (countProperty) => {
-    const count = books.reduce((counter, book) => {
+    const count = booksData.reduce((counter, book) => {
         counter[book[countProperty]] = (counter[book[countProperty]] || 0) + 1;
         return counter;
     }, {});
     return formatResponse({ code: 200, data: count });
 };
 
-// Helper function to remove an element from an array
-const removeElement = (array, predicate) => {
-    const index = array.findIndex(predicate);
-    if (index === -1) return [null, array];
-    return [array.splice(index, 1)[0], array];
-};
-
 // Test functions
-console.log(getBook("111"));
+console.log(getBook("9781408855652"));
 console.log(getBooks());
-console.log(addBook({ title: "Oaxaqueños", author: "Mond Oaxaca", year: 2022 }));
-console.log(removeBookByTitleOrISBN("111"));
-console.log(filterBy("genre", "Fiction"));
+console.log(addBook({ title: "New Book", ISBN: "1234567890", year: 2024 }));
+console.log(removeBookByTitleOrISBN("9781408855652"));
+console.log(filterBy("genre", "Fantasy"));
 console.log(listBooks());
-console.log(getBooksByYear(2022));
-console.log(genreFullAvailability("Fiction"));
+console.log(getBooksByYear(2018));
+console.log(genreFullAvailability("Self-help"));
 console.log(genrePartialAvailability("Non-Fiction"));
 console.log(getCountBy("genre"));
